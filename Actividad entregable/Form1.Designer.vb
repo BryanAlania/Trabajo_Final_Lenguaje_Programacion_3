9786ebfacd1982bfc565136a407d76c2a0dea011
BryanAlania <1359537@senati.pe>
2022-10-26 01:15:23 -0500

    Actualizando, si a esta hora resolvi problemas con mi laptop

--- Actividad entregable/Form1.Designer.vb
+++ Actividad entregable/Form1.Designer.vb
@@ -27,6 +27,10 @@ Partial Class Form1
         Me.Label3 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -34,7 +38,7 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Label1.Location = New System.Drawing.Point(61, 61)
+        Me.Label1.Location = New System.Drawing.Point(61, 57)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(208, 32)
         Me.Label1.TabIndex = 0
@@ -45,7 +49,7 @@ Partial Class Form1
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Label2.Location = New System.Drawing.Point(61, 155)
+        Me.Label2.Location = New System.Drawing.Point(61, 110)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(30, 18)
         Me.Label2.TabIndex = 1
@@ -56,7 +60,7 @@ Partial Class Form1
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.Label3.Location = New System.Drawing.Point(61, 266)
+        Me.Label3.Location = New System.Drawing.Point(61, 194)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(104, 15)
         Me.Label3.TabIndex = 2
@@ -66,7 +70,7 @@ Partial Class Form1
         '
         Me.TextBox1.BackColor = System.Drawing.SystemColors.ScrollBar
         Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox1.Location = New System.Drawing.Point(61, 176)
+        Me.TextBox1.Location = New System.Drawing.Point(61, 131)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(225, 33)
         Me.TextBox1.TabIndex = 3
@@ -75,10 +79,39 @@ Partial Class Form1
         '
         Me.TextBox2.BackColor = System.Drawing.SystemColors.ScrollBar
         Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox2.Location = New System.Drawing.Point(61, 284)
+        Me.TextBox2.Location = New System.Drawing.Point(61, 212)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(225, 33)
         Me.TextBox2.TabIndex = 4
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Actividad_entregable.My.Resources.Resources._170041
+        Me.PictureBox1.Location = New System.Drawing.Point(469, 32)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(122, 152)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(458, 363)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 48)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "SALIR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(90, 317)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 48)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "registrar"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -86,6 +119,9 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.MidnightBlue
         Me.ClientSize = New System.Drawing.Size(661, 436)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label3)
@@ -93,6 +129,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -103,4 +140,7 @@ Partial Class Form1
     Friend WithEvents Label3 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- Actividad entregable/Form1.vb
+++ Actividad entregable/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Msg As MsgBoxResult
+        Msg = MsgBox("Cerrar la aplicacion,¿Desea salir?  ", vbYesNo, "Salir del sistema")
+        If Msg = MsgBoxResult.Yes Then
+            Application.ExitThread()
+        Else
+            Exit Sub
 
+        End If
+
+    End Sub
 End Class
